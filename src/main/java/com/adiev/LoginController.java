edb30b405dca3f924e8055afaa6409a1dc92c190
Adiev <com.adiev@gmail.com>
2024-11-10 08:31:43 +0800

    first commit

---
+++ src/main/java/com/adiev/LoginController.java
@@ -0,0 +1,36 @@
+package com.adiev;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("error", true);
+            model.addAttribute("errorMessage", "Invalid username or password");
+        }
+
+        if (logout != null) {
+            model.addAttribute("logout", true);
+            model.addAttribute("logoutMessage", "You have been logget out successfully");
+        }
+
+        CsrfToken csrf = (CsrfToken)  request.getAttribute(CsrfToken.class.getName());
+        if (csrf != null) {
+            model.addAttribute("csrf", csrf);
+        }
+
+        return "pages/login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "pages/home";
+    }
+}
